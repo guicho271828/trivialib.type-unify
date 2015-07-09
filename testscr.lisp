40b2c06fa7793eace5e459bc56dccb3653ecf872
Masataro Asai <guicho2.71828@gmail.com>
2015-07-09 09:18:12 +0900

    quickload

--- testscr.lisp
+++ testscr.lisp
@@ -1,10 +1,9 @@
 
 (in-package :cl-user)
-(ql:quickload '(:alexandria :trivia :introspect-environment :type-r))
 
 (uiop:quit (if (handler-case
                    (progn
-                     (asdf:load-system :trivialib.type-unify.test)
+                     (ql:quickload :trivialib.type-unify.test)
                      (eval (read-from-string "(every #'fiveam::TEST-PASSED-P (5am:run! :trivialib.type-unify))"))
 )
                  (serious-condition (c)
